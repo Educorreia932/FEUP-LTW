73951e2ebccea3b5a456fb5e705e4748b3b60bd5
Diogo Rosário <diogogrosario1008@gmail.com>
2020-11-27 12:37:57 +0000

    pet_profile page

--- src/user_profile.php
+++ src/user_profile.php
@@ -1,5 +1,8 @@
 <?php
-    session_start();
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
 
     include_once("templates/common/header.php");
     include_once("templates/common/footer.php");
