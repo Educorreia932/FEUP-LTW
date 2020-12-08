f47338a811496e09fbaf62d8319588fad7ee6ec7
Educorreia932 <skelozard1@gmail.com>
2020-12-08 01:01:56 +0000

    Fix: Image pathing issues

--- src/templates/pet_profile_comments.php
+++ src/templates/pet_profile_comments.php
@@ -1,6 +1,7 @@
 <script defer src="scripts/comments.js"></script>
 
 <div id="pet_profile_comments">
+    <!-- TODO: Only show if user is logged in -->
     <form method="post" onsubmit="submitComment(event)">
         <label class="input-comment">
             Add a new comment! <textarea id="commentText" name="text" rows="5"></textarea> 
--- start.bat
+++ start.bat
@@ -1,4 +1,1 @@
-sqlite3 src/database/pets.db < src/database/database_scheme.sql
-sqlite3 src/database/pets.db < src/database/populate_database.sql
- 
 cd src & php -S localhost:8000
