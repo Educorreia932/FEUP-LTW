485a533d6f51f035218ce1b3eff387e3c315e8ac
Henrique <gamingwithapotato@gmail.com>
2020-12-12 18:37:38 +0000

    Added url encode to user profile's get

--- src/templates/common/header.php
+++ src/templates/common/header.php
@@ -36,7 +36,7 @@
                 if (array_key_exists('username', $_SESSION) && !empty($_SESSION['username'])) {
                     $user = getUser($_SESSION['username'], $_SESSION['password']);
             ?>
-            <p>Greetings, <a href="<?="../pages/user_profile.php?user=" . $_SESSION['username']?>"><?= $_SESSION['username']?></a></p>
+            <p>Greetings, <a href="<?="../pages/user_profile.php?user=" .urlencode($_SESSION['username'])?>"><?= $_SESSION['username']?></a></p>
             
             <script src="../scripts/user_menu.js"></script>
             
@@ -44,7 +44,7 @@
             
             <div id="avatar_dropdown" style="display: none">
                 <ul>
-                    <li><a href="<?="../pages/user_profile.php?user=" . $_SESSION['username']?>">Profile</a></li>
+                    <li><a href="<?="../pages/user_profile.php?user=" . urlencode($_SESSION['username'])?>">Profile</a></li>
                     <li><a href="../pages/settings.php">Settings</a></li>
                     <li><a href="../actions/log_out.php">Log Out</a></li>
                 </ul>
