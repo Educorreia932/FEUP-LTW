fcb58595d1536deda40358ea4eb2a2d12ac85ffb
Diogo Rosário <diogogrosario1008@gmail.com>
2020-12-13 21:24:20 +0000

    edit profile working, needs css fix on edit_post_form.php

---
+++ src/actions/action_edit_post.php
@@ -0,0 +1,27 @@
+<?php
+    include_once(__DIR__ . "/../config.php");
+
+    if (session_status() == PHP_SESSION_NONE){
+        session_set_cookie_params(0, '/', $_SERVER['HTTP_HOST'], true, true);
+        session_start(); 
+        if (!isset($_SESSION['csrf'])) {
+            $_SESSION['csrf'] = bin2hex(openssl_random_pseudo_bytes(32));
+          }
+    } 
+
+    include_once(ROOT . "/database/connection.php"); 
+    include_once(ROOT . "/database/pets.php");     
+
+    $post_title = $_POST['post_title'];
+    $location = $_POST['location'];
+    $description = $_POST['description'];
+    $post_id = $_POST['adoption_post_id'];
+    var_dump($post_title);
+    var_dump($location);
+    var_dump($description);
+    var_dump($post_id);
+
+    edit_post($post_title, $location, $description, $post_id);
+
+    header("Location: ../pages/adoption_post.php?id=$post_id");
+?>
---
+++ src/templates/edit_post.php
@@ -0,0 +1,45 @@
+<script defer src="../scripts/addProposal.js"></script>
+<?php
+    $author_name = getUserById($adoption_post['AuthorID']);
+?>
+<div id="pet_profile">
+    <h1>
+        <?= $adoption_post["Title"] ?>
+    </h1>
+    <h4 id="author">
+        by <?=$author_name?>
+    </h4>
+    <h2>
+        <img id="front" src="../<?= $pet["Photo"] ?>" alt="Pet Photo">
+        <img id="background" src="../<?= $pet["Photo"] ?>" alt="Pet Photo">
+    </h2>
+
+    <div id="grid">
+        <div id="pet_info">
+            <h3>
+                <?= $pet["Name"] ?>
+            </h3>
+
+            <p>
+                <?= $adoption_post["Location"] ?>
+            </p>
+
+            <hr>
+            
+            <p id="info_bullets">
+                <a> <?= htmlspecialchars(getSpeciesName($pet["SpeciesID"])) ?> </a>
+                <a> <?= htmlspecialchars(getGender($pet["Gender"])) ?> </a>
+                <a> <?= $pet["Age"] ?> </a> 
+            </p>
+            
+            <hr>
+            
+            <p>
+                About
+            </p>
+
+            <p>
+                <?= $adoption_post["Description"] ?>
+            </p>
+        </div>
+</div>
