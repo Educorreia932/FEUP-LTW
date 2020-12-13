01cde76f75260b3c6878a46ee4d8474a6d2ef1ea
Henrique <gamingwithapotato@gmail.com>
2020-12-13 19:02:55 +0000

    Removed csrf token from helpershelter in header

--- src/templates/common/header.php
+++ src/templates/common/header.php
@@ -31,7 +31,7 @@
 
     <body>
         <header>
-            <h1><a href="../"><?php echo("Helper Shelter" . $_SESSION["csrf"] )?></div></a></h1>
+            <h1><a href="../"><?php echo("Helper Shelter")?></div></a></h1>
             <div id="authentication">  
 
             <?php
